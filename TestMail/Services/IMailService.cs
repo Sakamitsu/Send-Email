eaa76d7590b4037a67c20aaca15596117c2df836
Sakamitsu <86984855+Sakamitsu@users.noreply.github.com>
2022-03-15 17:56:06 +0300

    Add project files.

---
+++ TestMail/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestMail.Models;
+
+namespace TestMail.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
